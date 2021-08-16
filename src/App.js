d8c9505033d4c3ddb19b85d6ef7c1a61978dea9b
Giancarlo Guzman Oki <giancarloguzo@gmail.com>
2021-08-15 20:18:02 -0500

    Adds SignUp form

--- src/App.js
+++ src/App.js
@@ -4,17 +4,44 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import SignUpForm from "./SignupForm";
 
-import matches from "./constants/matches"
+import matches from "./constants/matches";
 
 function App() {
-  console.log('matches: ', matches);
+
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [user, setUser] = useState({});
+  const isUserLogged = user?.id !== undefined;
+
+  const handleSignUp = (user) => {
+    setUser(user);
+    handleClose();
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+  }
 
   return (
     <Container>
+      <Modal show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>Login</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <SignUpForm handleSignUp={handleSignUp} />
+        </Modal.Body>
+      </Modal>
       <Row className="mb-4 mt-2">
-        <Col className="text-right">
-          <Button>Login button</Button>
+        <Col className="d-flex justify-content-end align-items-center">
+          <p className="mb-0 mr-2">{isUserLogged ? `Hello, ${user.email}` : ""}</p>
+          {isUserLogged ? (<Button onClick={logout}>Logout</Button>) : (<Button onClick={handleShow}>Sign up</Button>)}
         </Col>
       </Row>
       <Row>
@@ -23,15 +50,16 @@ function App() {
         </Col>
       </Row>
       <Row className="mb-3">
-        {matches.map((match) =>
-          <Col xs={6} className="mb-3">
+        {matches.map((match, index) => (
+          <Col key={index} xs={6} className="mb-3">
             <Card>
               <Card.Body className="mx-auto">
                 <Button className="mr-5">{match.team1}</Button>
                 <Button>{match.team2}</Button>
               </Card.Body>
             </Card>
-          </Col>)}
+          </Col>
+        ))}
       </Row>
     </Container>
   );
