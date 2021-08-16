08c0b1e367b73cfcb23d58ff98d59f5f711ccf75
Giancarlo Guzman Oki <giancarloguzo@gmail.com>
2021-08-15 20:10:27 -0500

    Adds bootstrap and index mockup view

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+
+import matches from "./constants/matches"
 
 function App() {
+  console.log('matches: ', matches);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row className="mb-4 mt-2">
+        <Col className="text-right">
+          <Button>Login button</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="mb-3">
+          <h1 className="text-center">Pick'em</h1>
+        </Col>
+      </Row>
+      <Row className="mb-3">
+        {matches.map((match) =>
+          <Col xs={6} className="mb-3">
+            <Card>
+              <Card.Body className="mx-auto">
+                <Button className="mr-5">{match.team1}</Button>
+                <Button>{match.team2}</Button>
+              </Card.Body>
+            </Card>
+          </Col>)}
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/constants/matches.js
@@ -0,0 +1,46 @@
+const matches = [
+  {
+    team1: "team 1",
+    team2: "team 2",
+    date: "2021-05-04",
+    status: "pending",
+    result: null,
+  },
+  {
+    team1: "team 2",
+    team2: "team 3",
+    date: "2021-04-03",
+    status: "pending",
+    result: null,
+  },
+  {
+    team1: "team 3",
+    team2: "team 4",
+    date: "2021-05-06",
+    status: "pending",
+    result: null,
+  },
+  {
+    team1: "team 5",
+    team2: "team 2",
+    date: "2021-05-15",
+    status: "pending",
+    result: null,
+  },
+  {
+    team1: "team 4",
+    team2: "team 2",
+    date: "2021-06-10",
+    status: "pending",
+    result: null,
+  },
+  {
+    team1: "team 5",
+    team2: "team 1",
+    date: "2021-05-17",
+    status: "pending",
+    result: null,
+  },
+]
+
+export default matches
